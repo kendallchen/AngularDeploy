8cdcbed1275ac852da0135013c61b8eb88105f95
support <support@dotnetlead.com>
2021-05-04 20:25:57 -0700

    add project

---
+++ api/Data/insertCustomer.sql
@@ -0,0 +1,7 @@
+﻿insert into customer(firstname, lastname) values ('Cassandra','Eleanora');
+insert into customer(firstname, lastname) values ('Mauricio','Kelli');
+insert into customer(firstname, lastname) values ('Gracia','Ronny');
+insert into customer(firstname, lastname) values ('Randolph','Marvella');
+insert into customer(firstname, lastname) values ('Elke','Joseph');
+insert into customer(firstname, lastname) values ('Wilfred','Sulema');
+insert into customer(firstname, lastname) values ('Patrina','Sheridan');
